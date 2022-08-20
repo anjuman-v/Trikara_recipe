5d80158f240dd6e521f0f477625b083d364d47e4
anjuman-v <ansa.anjuman00786@gmail.com>
2022-08-20 16:33:00 +0530

    1-1

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -58,42 +58,14 @@ router.patch("/recipe/:id", auth, async (req, res) => {
 
 
 router.get('/recipes', auth,  async(req, res) => {
-    try{
-        const page = req.query.page || 1
-        const limit = req.query.limit || 10
-        let totalPages = 0;
-        let recipes
-        if(req.query.q) {
-           if(req.query.q == 'filter'){
-                recipes = await Recipe.find({name: req.query.base}).skip((page - 1) * limit).limit(limit).lean().exec()
-                const totalPage = await Recipe.find({name: req.query.base}).countDocuments()
-                totalPages = (Math.ceil(totalPage/limit))
-            }
-
-            else
-                {
-                    recipes = await Recipe.find({block : req.query.block}).skip((page - 1) * limit).limit(limit).lean().exec()
-                    const totalPage = await Recipe.find({block : req.query.block}).countDocuments()
-                    totolPages = (Math.ceil(totalPage/limit))
-                }
-        }
-        else{
-            recipes = await Recipe.find().skip((page - 1) * limit).limit(limit).lean().exec()
-            const totalPage = await Recipe.find().countDocuments()
-            totalPages = (Math.ceil(totalPage/limit))
-
-        }
-
-        let arr = []
-        for(let i = 1; i <= totalPages; i++){
-            arr.push(i)
-
-        }
-        return res.status(200).send({recipes, totalPages:arr})
-    } 
-    catch (error) {
-        return req.status(500).send({message : error.message})
-    }
+  
+    try {
+        const recipe = await Recipe.find().lean().exec();
+    
+        res.status(200).send(recipe);
+      } catch (error) {
+        return res.status(500).send(error.message);
+      }
 })
 
 
