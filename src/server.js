91110393048e0abfc46411a17eeec234160b62a9
anjuman-v <ansa.anjuman00786@gmail.com>
2022-08-21 16:15:38 +0530

    1-1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
-// console.log("hii")
 const express = require('express')
-// const cors = require('cors')
+ const cors = require('cors')
 
 const mongoDBConnector = require('./configs/db')
 
@@ -10,9 +9,15 @@ const recipeController  = require('./controllers/recipe.controller');
 
 const app = express()
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 const port = process.env.PORT || 5000
 
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,           
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use('/signup', register)
 app.use('/signin', login)
