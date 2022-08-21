c50b5297800bc78c369b9a4384bccc4d51fed0fd
anjuman-v <ansa.anjuman00786@gmail.com>
2022-08-21 19:16:56 +0530

    1-0

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//  const cors = require('cors')
+ const cors = require('cors')
 
 const mongoDBConnector = require('./configs/db')
 
@@ -13,12 +13,12 @@ app.use(express.json())
 
 const port = process.env.PORT || 5000
 
-// const corsOptions ={
-//     origin:'https://trikara-recipe-client-anzy.vercel.app', 
-//     credentials:true,           
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const corsOptions ={
+    origin:'https://trikara-recipe-client-anzy.vercel.app', 
+    credentials:true,           
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use('/signup', register)
 app.use('/signin', login)
