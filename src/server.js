d7ef2f570eb71afdaa0afcd45db8d19c88f24712
anjuman-v <ansa.anjuman00786@gmail.com>
2022-09-15 13:45:05 +0530

    1-3

--- src/server.js
+++ src/server.js
@@ -14,12 +14,13 @@ app.use(express.json())
 const port = process.env.PORT || 5000
 //
 //cors 
-const corsOptions ={
-    origin:'http://localhost:3000/', 
-    credentials:true,           
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000/', 
+//     credentials:true,           
+//     optionSuccessStatus:200
+// }
+
+app.use(cors());
 
 app.use('/signup', register)
 app.use('/signin', login)
